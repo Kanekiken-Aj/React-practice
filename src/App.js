6c483ebe26dc460ec00bde50c715cf9f420c6279
Kanekiken-Aj <achintya.developer@gmail.com>
2023-09-29 17:22:54 +0530

    Added multi-page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
 import { useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-// import{
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   // Link
-// } from "react-router-dom";
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
 
 
 
@@ -49,26 +49,26 @@ function App() {
   }
   return (
     <>
-    {/* <Router> */}
+    <Router>
       {/* <Navbar title="TextUtils" about="About TextUtils?"/> */}
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>
         <div className='container my-3'>
-          {/* <Routes>
+          <Routes>
             {/* /users --> Component 1 */}
             {/* /user/homes ---> ---> Component 2 */}
 
-            {/* <Route exact path="/about" element={<About/>}>  */}
-            {/* </Route>   */} 
-            {/* <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze"  mode={mode} />}> */}
-           <TextForm showAlert={showAlert} heading="Enter the text to analyze"  mode={mode} />
+            <Route exact path="/about" element={<About/>}> 
+            </Route>   
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze"  mode={mode} />}>
+              {/* <TextForm showAlert={showAlert} heading="Enter the text to analyze"  mode={mode} /> */}
 
-            {/* </Route> 
-          </Routes>; */}
+            </Route> 
+          </Routes>
           
         
         </div>
-      {/* </Router> */}
+      </Router>
     </>
   );
 }
